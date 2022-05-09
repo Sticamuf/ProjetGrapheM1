29c305cfdd93b38d0fa1972ef0cacb8491bada76
AllKings <36733505+AllKings@users.noreply.github.com>
2022-05-09 12:15:00 +0200

    Create IntersectionEdge.cpp

---
+++ ProjetGrapheM1/IntersectionEdge.cpp
@@ -0,0 +1,13 @@
+#include <ogdf/basic/GraphAttributes.h>
+
+using ogdf::edge;
+using ogdf::GraphAttributes;
+using ogdf::node;
+using ogdf::DPolyline;
+using ogdf::ListIterator;
+using ogdf::DPoint;
+
+bool intersection(const edge& e1,const edge& e2)
+{
+  
+}
