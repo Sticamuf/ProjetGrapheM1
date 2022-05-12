236c8a2136fa3a2b59682f55faff8a8b3b868450
AllKings <36733505+AllKings@users.noreply.github.com>
2022-05-12 17:10:43 +0200

    Update IntersectionEdge.cpp

--- ProjetGrapheM1/IntersectionEdge.cpp
+++ ProjetGrapheM1/IntersectionEdge.cpp
@@ -34,14 +34,14 @@ bool estDansVector(const edge& e)
 	}
 }
 
-bool surSegment(int px, int py, int qx, int qy, int rx, int ry)
+/*bool surSegment(int px, int py, int qx, int qy, int rx, int ry)
 {
 	if (qx <= max(px, rx) && qx >= min(px, rx) && qy <= max(py, ry) && qy >= min(py, ry))
 	{
 		return true;
 	}
     	return false;
-}
+}*/
 
 bool sontAlignes(int px, int py, int qx, int qy, int rx, int ry) // pour vérifier si c'est aligné
 {
@@ -50,7 +50,29 @@ bool sontAlignes(int px, int py, int qx, int qy, int rx, int ry) // pour vérifi
 
 bool Alignes(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 {
-		
+	if(sontAlignes(px,py,qx,qy,rx,ry)==true)
+	{
+		align=1;
+		return true;
+	}
+	else if(sontAlignes(px,py,qx,qy,sx,sy)==true)
+	{
+		align=2;
+		return true;
+	}
+	else if(sontAlignes(rx,ry,sx,sy,px,py)==true)
+	{
+		align=3;
+		return true;
+	}
+	else if(sontAlignes(rx,ry,sx,sy,qx,qy)==true)
+	{
+		align=4;
+		return true;
+	}
+	align=-1;
+	return false;
+		   
 }
 
 bool estADroite(int px,int rx)
@@ -89,18 +111,91 @@ bool estEnBas(int py,int ry)
 	return false;
 }
 
-int orientation(int px, int py, int qx, int qy, int rx, int ry)
+/*int orientation(int px, int py, int qx, int qy, int rx, int ry)
 {
     long long val = ((qy - py) * (rx - qx)) - ((qx - px) * (ry - qy));
  
     if (val == 0) return 0;  // collinéaire
  
     return (val > 0)? 1: 2; // dans le sens des aiguilles d'une montre ou contraire
+}*/
+
+bool seCroisent(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
+{
+	if(estADroite(px,rx)==true&&estAGauche(qx,rx)==true&&
+			   estEnHaut(py,ry)&&estEnHaut(qy,ry)&&
+			   estEnBas(py,sy)&&estEnBas(qy,sy))
+	{
+		return true;	
+	}
+}
+
+bool Croisement(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
+{
+	if(Alignes(px,py,qx,qy,rx,ry,sx,sy)==true)
+	{
+		if(align==1)
+		{
+			if((estAGauche(px,sx)==true&&estAGauche(qx,sx)==true)||
+			   (estADroite(px,sx)==true&&estADroite(qx,sx)==true)||
+			   (estEnHaut(py,sy)==true&&estEnHaut(qy,sy)==true)||
+			   (estEnBas(py,sy)==true&&estEnBas(qy,sy)==true))
+			{
+				return true;	
+			}
+			else
+			{
+				return false;
+			}
+					
+		}
+		else if(align==2)
+		{
+			if()
+			{
+				return true;	
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if(align==3)
+		{
+			if()
+			{
+				return true;	
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if(align==4)
+		{
+			if()
+			{
+				return true;	
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	else
+	{
+		if(seCroisent(px,py,qx,qy,rx,ry,sx,sy)==true)
+		{
+			return true;	
+		}
+	}	
 }
 
 bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
 {
 	bool inter=false;
+	align=-1;
 	
   
 	node source1 = e1->source();
@@ -124,23 +219,12 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
 	DPolyline bends1 = GA.bends(e1);
 	DPolyline bends2 = GA.bends(e2);
 	if (bends1.size() == 0 && bends2.size()==0) {
-        	if(sontAlignes(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2)==true ||
-		   sontAlignes(sourceX1,sourceY1,targetX1,targetY1,targetX2,targetY2)==true || 
-		   sontAlignes(sourceX2,sourceY2,targetX2,targetY2,sourceX1,sourceY1)==true || 
-		   sontAlignes(sourceX2,sourceY2,targetX2,targetY2,targetX1,targetY1)==true)
-		{
-				
-		}
-		else
-		{
-			if(estADroite(sourceX1,sourceX2)==true&&estAGauche(targetX1,sourceX2)==true&&
-			   estEnHaut(sourceX1,sourceX2)&&estEnHaut(targetX1,sourceX2)&&
-			   estEnBas(sourceX1,targetX2)&&estEnBas(targetX1,targetX2))
-			{
-				inter=true;	
-			}
-		}
+        	
     	}
+	else
+	{
+		
+	}
 	
 	/*int o1 = orientation(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2);
     	int o2 = orientation(sourceX1,sourceY1,targetX1,targetY1,targetX2,targetY2);
