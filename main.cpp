29196db41a8c9d07abb5ff1be5b3ffb588ec58b2
JulienSluggly <bianchetti.jul@gmail.com>
2022-05-18 18:28:47 +0200

    Embedder avec les bends

--- main.cpp
+++ main.cpp
@@ -21,22 +21,22 @@ const int Q4 = 3;
 //renvoie 1,2,3 ou 4 si la node targ est 1: en haut à droite, 2: en haut à gauche, 3: en bas à gauche, 4: en bas à droite de la node src 
 //dans le layout GL
 //on considère src != targ
-int quadrant(const GridLayout& GL, const node& src, const node& targ) {
-    if (GL.x(targ) > GL.x(src)) {
-        if (GL.y(targ) >= GL.y(src)) {
+int quadrant(int sx, int sy, int tx, int ty) {
+    if (tx > sx) {
+        if (ty >= sy) {
             return Q1;
         }
         return Q4;
     }
-    if (GL.y(targ) > GL.y(src)) {
+    if (ty > sy) {
         return Q2;
     }
     return Q3;
 }
 
 //on considère dans le meme quadrant
-bool aGauche(const GridLayout& GL, const node& src, const node& targ, const node& comp) {
-    return ((GL.x(targ) - GL.x(src)) * (GL.y(comp) - GL.y(src)) - (GL.y(targ) - GL.y(src)) * (GL.x(comp) - GL.x(src))) > 0;
+bool aGauche(int sx, int sy, int tx, int ty, int cx, int cy) {
+    return ((tx - sx) * (cy - sy) - (ty - sy) * (cx - sx)) > 0;
 }
 
 void embedderDeFou(Graph& G, GridLayout& GL) {
@@ -49,32 +49,70 @@ void embedderDeFou(Graph& G, GridLayout& GL) {
     SListIterator<adjEntry> it2;
     // Itérateur égal a it2-1 :)
     SListIterator<adjEntry> it3;
-    int i = 0;
+    node ntrg;
+    // Coord x,y du node source
+    int sx, sy;
+    // Coor x,y du node target
+    int tx, ty;
+    // Coor x,y du nouveau node a ajouter
+    int nx, ny;
     while (nsrc != nullptr) {
+        sx = GL.x(nsrc);
+        sy = GL.y(nsrc);
         nsrc->allAdjEntries(adj);
         newOrder.clear(); 
         it = adj.begin();
         // On insere le premier element et on passe directement au prochain
         newOrder.pushBack((*it));
         it++;
-        std::cout << "Tour: " << i << std::endl;
-        std::cout << "Nb Adj: " << adj.size() << std::endl;
         // On itere sur tout les éléments de la liste non triée
-        int j = 0;
         for (;it.valid();it++) {
             bool inserted = false;
             // On itere sur le deuxieme tableau tant qu'on est pas a la fin et tant qu'on a pas inseré
-            std::cout << "Tour interne: " << j << std::endl;
-            j++;
-            int k = 0;
             for (it2 = newOrder.begin(),it3=it2;((it2.valid())&&(!inserted));it3=it2++) {
-                std::cout << "Tour interne interne: " << k << std::endl;
-                std::cout << "Size New Order interne interne: " << newOrder.size() << std::endl;
-                k++;
-                node ntrg = (*it2)->twinNode();
-                int qtrg = quadrant(GL, nsrc, ntrg);
-                node newnode = (*it)->twinNode();
-                int qnewnode = quadrant(GL, nsrc, newnode);
+                edge tmpEdge = (*it2)->theEdge();
+                IPolyline& p = GL.bends(tmpEdge);
+                // Si l'edge contient des bends
+                if (p.size() > 0) {
+                    // Si le noeud source est le meme, on prend le premier bend
+                    if (tmpEdge->source() == nsrc) {
+                        tx = p.front().m_x;
+                        ty = p.front().m_y;
+                    }
+                    // Sinon on prend le dernier bend
+                    else {
+                        tx = p.back().m_x;
+                        ty = p.back().m_y;
+                    }
+                }
+                // Si pas de bends on prends les coordonnées du noeud
+                else {
+                    ntrg = (*it2)->twinNode();
+                    tx = GL.x(ntrg);
+                    ty = GL.y(ntrg);
+                }
+                // Quadrant du noeud/premier bend
+                int qtrg = quadrant(sx,sy,tx,ty);
+
+
+                edge tmpEdge2 = (*it)->theEdge();
+                IPolyline& p2 = GL.bends(tmpEdge2);
+                if (p2.size() > 0) {
+                    if (tmpEdge2->source() == nsrc) {
+                        nx = p2.front().m_x;
+                        ny = p2.front().m_y;
+                    }
+                    else {
+                        nx = p2.back().m_x;
+                        ny = p2.back().m_y;
+                    }
+                }
+                else {
+                    node newnode = (*it)->twinNode();
+                    nx = GL.x(newnode);
+                    ny = GL.y(newnode);
+                }
+                int qnewnode = quadrant(sx,sy,nx,ny);
                 // Si le quadrant du point que l'on veut inserer est inférieur a celui qu'on compare
                 if (qnewnode < qtrg) {
                     // Si on compare au premier, on insere en premiere place
@@ -92,7 +130,7 @@ void embedderDeFou(Graph& G, GridLayout& GL) {
                     // Tant que les quadrants sont égaux
                     while ((qnewnode == qtrg) && (!inserted)) {
                         // Si on est a droite on insere apres it3
-                        if (!aGauche(GL, nsrc, ntrg, newnode)) {
+                        if (!aGauche(sx,sy,tx,ty,nx,ny)) {
                             if (it2 == newOrder.begin()) {
                                 newOrder.pushFront((*it));
                             }
@@ -106,8 +144,24 @@ void embedderDeFou(Graph& G, GridLayout& GL) {
                         else {
                             it3 = it2++;
                             if (it2 != newOrder.end()) {
-                                ntrg = (*it2)->twinNode();
-                                qtrg = quadrant(GL, nsrc, ntrg);
+                                edge tmpEdge3 = (*it2)->theEdge();
+                                IPolyline& p3 = GL.bends(tmpEdge3);
+                                if (p3.size() > 0) {
+                                    if (tmpEdge->source() == nsrc) {
+                                        tx = p3.front().m_x;
+                                        ty = p3.front().m_y;
+                                    }
+                                    else {
+                                        tx = p3.back().m_x;
+                                        ty = p3.back().m_y;
+                                    }
+                                }
+                                else {
+                                    ntrg = (*it2)->twinNode();
+                                    tx = GL.x(ntrg);
+                                    ty = GL.y(ntrg);
+                                }
+                                qtrg = quadrant(sx, sy, tx, ty);
                             }
                             else {
                                 qnewnode = 5;
@@ -128,10 +182,8 @@ void embedderDeFou(Graph& G, GridLayout& GL) {
                 newOrder.pushBack((*it));
             }
         }
-        std::cout << "Size New Order Final: " << newOrder.size() << std::endl;
         G.sort(nsrc, newOrder);
         nsrc = nsrc->succ();
-        i++;
     }
 }
 
@@ -143,7 +195,7 @@ int main() {
     int gridWidth, gridHeight, maxBends;
 
     // ----- LECTURE D'UN FICHIER JSON DANS UN Graph -----
-    string file = "exemple7.json";
+    string file = "exemples/man3.json";
     readFromJson(file, G, GL, gridWidth, gridHeight, maxBends);
     writeToJson("output.json", G, GL, gridWidth, gridHeight, maxBends);
 
@@ -157,19 +209,15 @@ int main() {
         i++;
     }
 
-    std::cout << G.representsCombEmbedding() << std::endl;
+    
     int maxX = 0, maxY = 0, minX = 100000, minY = 100000;
 
-    embedderDeFou(G, GL);
-    std::cout << "Embeded: " << G.representsCombEmbedding() << std::endl;
-
     bool planarize = false;
-    std::cout << "Connexe: " << isConnected(G) << std::endl;
-    std::cout << "Planaire: " << isPlanar(G) << std::endl;
     if (planarize) {
+        std::cout << "Planarizing..." << std::endl;
         PlanarStraightLayout PL;
         PL.separation(-19);
-        PL.callGridFixEmbed(G, GL);
+        PL.callGrid(G, GL);
         node n = G.firstNode();
         while (n != nullptr) {
             if (GL.x(n) > maxX) maxX = GL.x(n);
@@ -178,8 +226,12 @@ int main() {
             if (GL.y(n) < minY) minY = GL.x(n);
             n = n->succ();
         }
-        std::cout << "Planarised" << std::endl;
     }
+
+    std::cout << "Embedding..." << std::endl;
+    embedderDeFou(G, GL);
+    std::cout << "Embeded: " << G.representsCombEmbedding() << std::endl;
+
     //GraphIO::write(GL, "output-ERDiagram2.svg", GraphIO::drawSVG);
     std::cout << "Connexe: " << isConnected(G) << std::endl;
     std::cout << "Planaire: " << isPlanar(G) << std::endl;
