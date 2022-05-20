4a6145bdd2e8a5be8944d2772b5700aa50133a95
AllKings <36733505+AllKings@users.noreply.github.com>
2022-05-20 08:59:27 +0200

    Update IntersectionEdge.cpp

--- ProjetGrapheM1/IntersectionEdge.cpp
+++ ProjetGrapheM1/IntersectionEdge.cpp
@@ -50,22 +50,22 @@ bool sontAlignes(int px, int py, int qx, int qy, int rx, int ry) // pour vérifi
 
 bool Alignes(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 {
-	if(sontAlignes(px,py,qx,qy,rx,ry)==true)
+	if(sontAlignes(px,py,qx,qy,rx,ry))
 	{
 		align=1;
 		return true;
 	}
-	else if(sontAlignes(px,py,qx,qy,sx,sy)==true)
+	else if(sontAlignes(px,py,qx,qy,sx,sy))
 	{
 		align=2;
 		return true;
 	}
-	else if(sontAlignes(rx,ry,sx,sy,px,py)==true)
+	else if(sontAlignes(rx,ry,sx,sy,px,py))
 	{
 		align=3;
 		return true;
 	}
-	else if(sontAlignes(rx,ry,sx,sy,qx,qy)==true)
+	else if(sontAlignes(rx,ry,sx,sy,qx,qy))
 	{
 		align=4;
 		return true;
@@ -75,7 +75,7 @@ bool Alignes(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 		   
 }
 
-bool estADroite(int px,int rx)
+bool EstSuperieurX(int px,int rx)
 {
 	if(px<rx)
 	{
@@ -84,7 +84,7 @@ bool estADroite(int px,int rx)
 	return false;
 }
 
-bool estAGauche(int px,int rx)
+bool EstInferieurX(int px,int rx)
 {
 	if(px>rx)
 	{
@@ -93,7 +93,7 @@ bool estAGauche(int px,int rx)
 	return false;
 }
 
-bool estEnHaut(int py,int ry)
+bool EstSuperieurY(int py,int ry)
 {
 	if(py<ry)
 	{
@@ -102,7 +102,7 @@ bool estEnHaut(int py,int ry)
 	return false;
 }
 
-bool estEnBas(int py,int ry)
+bool EstInferieurY(int py,int ry)
 {
 	if(py>ry)
 	{
@@ -150,14 +150,14 @@ bool seCroisent(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 
 bool Croisement(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 {
-	if(Alignes(px,py,qx,qy,rx,ry,sx,sy)==true)
+	if(Alignes(px,py,qx,qy,rx,ry,sx,sy))
 	{
 		if(align==1)
 		{
-			if((estAGauche(px,rx)==true&&estADroite(qx,rx)==true)||
-			   (estADroite(px,rx)==true&&estAGauche(qx,rx)==true)||
-			   (estEnHaut(py,ry)==true&&estEnBas(qy,ry)==true)||
-			   (estEnBas(py,ry)==true&&estEnHaut(qy,ry)==true))
+			if((EstInferieurX(px,rx)&&EstSuperieurX(qx,rx))||
+			   (EstSuperieurX(px,rx)&&EstInferieurX(qx,rx))||
+			   (EstSuperieurY(py,ry)&&EstInferieurY(qy,ry))||
+			   (EstInferieurY(py,ry)&&EstSuperieurY(qy,ry)))
 			{
 				return true;	
 			}
@@ -169,10 +169,10 @@ bool Croisement(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 		}
 		else if(align==2)
 		{
-			if((estAGauche(px,sx)==true&&estADroite(qx,sx)==true)||
-			   (estADroite(px,sx)==true&&estAGauche(qx,sx)==true)||
-			   (estEnHaut(py,sy)==true&&estEnBas(qy,sy)==true)||
-			   (estEnBas(py,sy)==true&&estEnHaut(qy,sy)==true))
+			if((EstInferieurX(px,sx)&&EstSuperieurX(qx,sx))||
+			   (EstSuperieurX(px,sx)&&EstInferieurX(qx,sx))||
+			   (EstSuperieurY(py,sy)&&EstInferieurY(qy,sy))||
+			   (EstInferieurY(py,sy)&&EstSuperieurY(qy,sy)))
 			{
 				return true;	
 			}
@@ -183,10 +183,10 @@ bool Croisement(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 		}
 		else if(align==3)
 		{
-			if((estAGauche(rx,px)==true&&estADroite(sx,px)==true)||
-			   (estADroite(rx,px)==true&&estAGauche(sx,px)==true)||
-			   (estEnHaut(ry,py)==true&&estEnBas(sy,py)==true)||
-			   (estEnBas(ry,py)==true&&estEnHaut(sy,py)==true))
+			if((EstInferieurX(rx,px)&&EstSuperieurX(sx,px))||
+			   (EstSuperieurX(rx,px)&&EstInferieurX(sx,px))||
+			   (EstSuperieurY(ry,py)&&EstInferieurY(sy,py))||
+			   (EstInferieurY(ry,py)&&EstSuperieurY(sy,py)))
 			{
 				return true;	
 			}
@@ -197,10 +197,10 @@ bool Croisement(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 		}
 		else if(align==4)
 		{
-			if((estAGauche(rx,qx)==true&&estADroite(sx,qx)==true)||
-			   (estADroite(rx,qx)==true&&estAGauche(sx,qx)==true)||
-			   (estEnHaut(ry,qy)==true&&estEnBas(sy,qy)==true)||
-			   (estEnBas(ry,qy)==true&&estEnHaut(sy,qy)==true)))
+			if((EstInferieurX(rx,qx)&&EstSuperieurX(sx,qx))||
+			   (EstSuperieurX(rx,qx)&&EstInferieurX(sx,qx))||
+			   (EstSuperieurY(ry,qy)&&EstInferieurY(sy,qy))||
+			   (EstInferieurY(ry,qy)&&EstSuperieurY(sy,qy)))
 			{
 				return true;	
 			}
@@ -212,7 +212,7 @@ bool Croisement(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 	}
 	else
 	{
-		if(seCroisent(px,py,qx,qy,rx,ry,sx,sy)==true)
+		if(seCroisent(px,py,qx,qy,rx,ry,sx,sy))
 		{
 			return true;	
 		}
@@ -221,7 +221,6 @@ bool Croisement(int px, int py, int qx, int qy, int rx, int ry, int sx, int sy)
 
 bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
 {
-	bool inter=false;
 	align=-1;
 	
   
@@ -247,9 +246,9 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
   
 		targetX2 = GA.x(target2);
 		targetY2 = GA.y(target2);
-        	if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2)==true)
+        	if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2))
 		{
-			inter=true;	
+			return true;	
 		}
     	}
 	else if (bends1.size() > 0 && bends2.size()>0)
@@ -261,15 +260,45 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
 			{
 				targetX2 = (*j).m_x;
             			targetY2 = (*j).m_y;
-				if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2)==true)
+				if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2))
 				{
-					inter=true;	
-				}
+					return true;	
+				)
+				int endX2=GA.x(target2);
+				int endY2=GA.y(target2);
+				int originX2=GA.x(source2);
+				int originY2=GA.y(source2);
+				if(Croisement(originX2,originY2,endX2,endY2,sourceX2,sourceY2,targetX2,targetY2))
+				{
+					if((endX2==targetX2&&endY2==targetY2)||(originX2==sourceX2&&originY2==sourceY2))
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}					
+				)
 				sourceX2 = targetX2;
             			sourceY2 = targetY2;
 			}
 			sourceX2 = GA.x(source2);
             		sourceY2 = GA.y(source2);
+					int endX1=GA.x(target1);
+				int endY1=GA.y(target1);
+				int originX1=GA.x(source1);
+				int originY1=GA.y(source1);
+				if(Croisement(originX1,originY1,endX1,endY1,sourceX1,sourceY1,targetX1,targetY1))
+				{
+					if((endX1==targetX1&&endY1==targetY1)||(originX1==sourceX1&&originY1==sourceY1))
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}					
+				)
             		sourceX1 = targetX1;
             		sourceY1 = targetY1;
         	}
@@ -278,9 +307,9 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
   
 		targetX2 = GA.x(target2);
 		targetY2 = GA.y(target2);
-        	if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2)==true)
+        	if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2))
 		{
-			inter=true;	
+			return true;	
 		}
 		
 	}
@@ -291,18 +320,33 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
 		for(ListIterator<DPoint> i = bends1.begin(); i.valid(); i++) {
 			targetX1 = (*i).m_x;
             		targetY1 = (*i).m_y;
-			if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2)==true)
+			if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2))
 			{
-				inter=true;	
+				return true;	
 			}
+			int endX2=GA.x(target2);
+				int endY2=GA.y(target2);
+				int originX2=GA.x(source2);
+				int originY2=GA.y(source2);
+				if(Croisement(originX2,originY2,endX2,endY2,sourceX2,sourceY2,targetX2,targetY2))
+				{
+					if((endX2==targetX2&&endY2==targetY2)||(originX2==sourceX2&&originY2==sourceY2))
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}					
+				)
 			sourceX1 = targetX1;
             		sourceY1 = targetY1;
 		}
 		targetX1 = GA.x(target1);
 		targetY1 = GA.y(target1);
-		if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2)==true)
+		if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2))
 		{
-			inter=true;	
+			return true;	
 		}
 	}
 	else
@@ -313,18 +357,33 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
 		{
 			targetX2 = (*j).m_x;
             		targetY2 = (*j).m_y;
-			if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2)==true)
+			if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2))
 			{
-				inter=true;	
+				return true;	
 			}
+			int endX1=GA.x(target1);
+				int endY1=GA.y(target1);
+				int originX1=GA.x(source1);
+				int originY1=GA.y(source1);
+				if(Croisement(originX1,originY1,endX1,endY1,sourceX1,sourceY1,targetX1,targetY1))
+				{
+					if((endX1==targetX1&&endY1==targetY1)||(originX1==sourceX1&&originY1==sourceY1))
+					{
+						return false;
+					}
+					else
+					{
+						return true;
+					}					
+				)
 			sourceX2 = targetX2;
             		sourceY2 = targetY2;
 		}
 		targetX2 = GA.x(target2);
 		targetY2 = GA.y(target2);
-        	if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2)==true)
+        	if(Croisement(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2,targetX2,targetY2))
 		{
-			inter=true;	
+			return true;	
 		}
 	}
 	
@@ -333,26 +392,34 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
     	// cas particuliers
     	if (o1 == 0 && surSegment(sourceX1,sourceY1,targetX1,targetY1,sourceX2,sourceY2))
 	{
-		inter=true;
+		return true;
 	}
  
     	if (o2 == 0 && surSegment(sourceX1,sourceY1,targetX1,targetY1,targetX2,targetY2))
 	{
-		inter=true;
+		return true;
 	}
  
     	if (o3 == 0 && surSegment(sourceX2,sourceY2,targetX2,targetY2,sourceX1,sourceY1))
 	{
-		inter=true;
+		return true;
 	}
  
     	if (o4 == 0 && surSegment(sourceX2,sourceY2,targetX2,targetY2,targetX1,targetY1))
 	{
-		inter=true;
+		return true;
 	}
 	*/
+	
+	
+	return false;
+
+}
+
+void ModidfierVecteur(const edge& e1,const edge& e2,const GraphAttributes& GA)
+{
 	// Si vraie on les ajoutent dans le vector s'ils n'y sont pas
-	if(inter==true)
+	if(intersection(e1,e2,GA))
 	{
 		if(estDansVector(e1)==false)
 		{
@@ -367,18 +434,16 @@ bool intersection(const edge& e1,const edge& e2,const GraphAttributes& GA)
 	{
 		posE1=-1;
 		posE2=-1;
-		if(estDansVector(e2)==true)
+		if(estDansVector(e2))
 		{
 			posE2=posE1;
 			in.erase(in.begin()+posE2);
 		}
-		if(estDansVector(e1)==true)
+		if(estDansVector(e1))
 		{
 			in.erase(in.begin()+posE1);
 		}
 
 
 	}
-	return inter;
-
 }
