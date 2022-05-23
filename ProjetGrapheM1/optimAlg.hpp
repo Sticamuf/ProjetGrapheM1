ed6e069c47bc20e2f4e1f5c5f98fe9176b25ef46
Adnan Mustafic <adnanmustafic00@gmail.com>
2022-05-23 17:34:05 +0200

    maj

--- ProjetGrapheM1/optimAlg.hpp
+++ ProjetGrapheM1/optimAlg.hpp
@@ -15,16 +15,13 @@ using ogdf::ListIterator;
 //la somme des carrés des longueurs divisé par le nombre de edge dans sommeVar (utilisée pour ne pas recalculer toute la somme a chaque changement)
 //la variance dans var
 void prepCalcVariance(double& moy, double& sommeVar, double& var) {
-	auto it = mapLengthEdgeSet.begin();
-	double temp = it->first * it->second.size();
-	moy = temp / mapEdgeLength.size(); //division en cours de route pour ne pas dépasser la taille des double
-	sommeVar = (it->first * temp) / mapEdgeLength.size(); //division en cours de route pour ne pas dépasser la taille des double
-	it++;
-	while (it != mapLengthEdgeSet.end()) {
-		temp = it->first * it->second.size();
-		moy += temp / mapEdgeLength.size();
+    double temp;
+    moy = 0; 
+	sommeVar = 0;
+    for (auto it = mapLengthEdgeSet.begin(); it != mapLengthEdgeSet.end(); it++) {
+		temp = it->first * it->second.size();//division en cours de route pour ne pas dépasser la taille des double
+		moy += temp / mapEdgeLength.size();//division en cours de route pour ne pas dépasser la taille des double
 		sommeVar += (it->first * temp) / mapEdgeLength.size();
-		it++;
 	}
 	calcVarianceEdgeLength(var, sommeVar, moy);
 }
@@ -42,28 +39,6 @@ void calcVarianceChang(double ancienLongEdge, double nouvLongEdge, double& moy, 
 
 	sommeVar -= (ancienLongEdge*ancienLongEdge) / mapEdgeLength.size();
 	sommeVar += (nouvLongEdge * nouvLongEdge) / mapEdgeLength.size();
-}
-
-unsigned long long calcTmpEdgeLengthSquared(const edge& e, int srcX, int srcY, const GridLayout& GL) {
-    node target = e->target();
-    unsigned long long length = 0.0;
-    int sourceX = srcX;
-    int sourceY = srcY;
-    int targetX, targetY;
-    IPolyline bends = GL.bends(e);
-    if (bends.size() > 0) {
-        for (ListIterator<IPoint> i = bends.begin(); i.valid(); i++) {
-            targetX = (*i).m_x;
-            targetY = (*i).m_y;
-            length += pow((targetX - sourceX), 2) + pow((targetY - sourceY), 2);
-            sourceX = targetX;
-            sourceY = targetY;
-        }
-    }
-    targetX = GL.x(target);
-    targetY = GL.y(target);
-    length += pow((targetX - sourceX), 2) + pow((targetY - sourceY), 2);
-    return length;
 }
 
 #endif OPTIM_ALG
